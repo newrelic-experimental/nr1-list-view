b693092ef6e9d2f29201f57a183ce956d58031d8
Amit Yathirajadasan <ayathirajadasan@newrelic.com>
2022-02-22 09:48:36 -0800

    refactor: move formatter files to their own folder

--- visualizations/list-view/formatters/b.js
+++ visualizations/list-view/formatters/b.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 const B = (num, opts) => {
   num = Number(num);
   if (!opts) return num;
@@ -12,11 +13,11 @@ const B = (num, opts) => {
   const vals = units.reduce(
     (acc, unit, u) => ({
       ...acc,
-      [u ? unit.charAt(0) + 'b' : 'b']: Math.pow(1024, u),
+      [u ? unit.charAt(0) + 'b' : 'b']: Math.pow(1024, u), // eslint-disable-line prefer-template
     }),
     {}
   );
-  const [_m, u1, u2] = opts.match(/[ ]*([kmgtp]?b)[ ]*>?[ ]*([kmgtp]?b)?[ ]*/i);
+  const [_m, u1, u2] = opts.match(/[ ]*([kmgtp]?b)[ ]*>?[ ]*([kmgtp]?b)?[ ]*/i); // eslint-disable-line no-unused-vars
   const u = (u2 || u1).toLowerCase();
   const bytes = u2 ? Math.floor(num * vals[u1]) : num;
   const unit = units[units.findIndex((uu) => uu.charAt(0) === u.charAt(0))];
